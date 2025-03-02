65dcc4e812c523612254ef714b97f8b588d0240b
yKaayo <caiokaea@gmail.com>
2025-03-01 22:01:01 -0300

    Change NPM to PNPM

--- src/components/Model3D.jsx
+++ src/components/Model3D.jsx
@@ -16,7 +16,7 @@ const Model3D = () => {
     const { width, height } = modelRef.current.getBoundingClientRect();
 
     const loader = new GLTFLoader();
-    loader.load("/bmw_g20_330i.glb", function (gltf) {
+    loader.load('/bmw_g20_330i.glb', function (gltf) {
       scene.add(gltf.scene);
       const model = gltf.scene;
 
@@ -34,7 +34,6 @@ const Model3D = () => {
           start: "top top",
           end: "bottom bottom",
           scrub: true,
-          markers: true,
         },
       });
 
@@ -42,7 +41,7 @@ const Model3D = () => {
         model.rotation,
         {
           x: Math.PI * 2,
-        y: Math.PI * 1.5,
+          y: Math.PI * 1.5,
         },
         0,
       );
@@ -62,7 +61,7 @@ const Model3D = () => {
           x: 0,
           y: "80%",
         },
-        0.7,
+        0.65,
       );
 
       tl.to(
@@ -115,13 +114,16 @@ const Model3D = () => {
     });
 
     const camera = new THREE.PerspectiveCamera(65, width / height, 0.1, 1000);
-    if (width < 375) {
-      camera.position.z = 10;
-    } else if (width >= 375 && width < 768) {
-      camera.position.z = 10.5;
-    } else if (width >= 768) {
-      camera.position.z = 4.5;
-    }
+
+    // if (width < 375) {
+    //   camera.position.z = 10;
+    // } else if (width >= 375 && width < 768) {
+    //   camera.position.z = 10.5;
+    // } else if (width >= 768) {
+    //   camera.position.z = 4.5;
+    // }
+
+    camera.position.z = 4.5;
 
     const light = new THREE.AmbientLight(0xffffff, 0.6);
     const directionalLight = new THREE.DirectionalLight(0xffffff, 5);
